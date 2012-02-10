9514d0a67f0bae0fdf1350e017c4eebc579e2b30
Christian Berkhoff <chris@acid.cl>
2012-02-10 12:04:45 -0300

    default locale

--- spec/guru.spec.js
+++ spec/guru.spec.js
@@ -1,33 +1,42 @@
 var Guru = require("../lib/guru.js");
 
 describe('guru', function() {
+  var defaultLocale = 'en-us';
+
 	var configuration = {
     storage: {
       key: 'language'
     },
     resources: {
-      defaultLocale: 'en-us',
+      defaultLocale: defaultLocale,
       path: './i18n/',
       serialisation: '.json'
     }
   };
 
 	var guru;
-  var enUsResource;
+  var defaultResource;
 
 	describe('Resources', function() {
 		it('should start flawlessly', function() {
       guru = new Guru(configuration);
       expect(guru).toBeDefined();
-      enUsResource = guru.ask(['en-us', 'en']);
-      expect(enUsResource).toBeDefined();
+      defaultResource = guru.ask([defaultLocale, 'en']);
+      expect(defaultResource).toBeDefined();
 		});
+
+    it('should not find an unregistered locale', function(){
+      crap = guru.ask(['es-cl']);
+      expect(crap).toBeDefined();
+      expect(crap.locale).toBeDefined();
+      expect(crap.locale).toBe(defaultLocale);
+    });
 	});
 
   describe('Translations', function(){
     it('should translate a simple key', function() {
-      expect(enUsResource.content).toBeDefined();
-      expect(enUsResource.content.index.headline).toBe('Hello (in en-US).');
+      expect(defaultResource.content).toBeDefined();
+      expect(defaultResource.content.index.headline).toBe('Hello (in en-US).');
     });
   });
 });
